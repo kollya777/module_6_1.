4115f76b3dbd8cc1d06f49121c7def75c290e8ec
kollya777 <kollya777@mail.ru>
2024-07-07 17:00:10 +0300

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,3 +1,11 @@
+# Вот почему они определены:
+# 
+# self.alive = True: Этот атрибут указывает на текущее состояние животного. При создании нового экземпляра Animal (и его подклассов) атрибут alive устанавливается в True, что означает, что животное живо.
+# 
+# self.fed = False: Этот атрибут используется для отслеживания того, было ли животное уже когда-либо покормлено. При создании экземпляра Animal (и его подклассов) атрибут fed устанавливается в False, что означает, что животное еще не было покормлено.
+# 
+# Эти атрибуты представляют состояние каждого животного, которое можно изменять в процессе его жизни, например, при выполнении методов, таких как eat в классе Animal.
+
 class Animal:
     def __init__(self, name):
         self.name = name
@@ -5,32 +13,37 @@ class Animal:
         self.fed = False
 
     def eat(self, food):
-        if not food.edible:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+        if food.edible:
 
-        else:
             print(f"{self.name} съел {food.name}")
             self.fed = True
 
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+
 class Plant:
     edible = False
+
     def __init__(self, name):
         self.name = name
 
 
 class Mammal(Animal):
     pass
+
+
 class Predator(Animal):
-        pass
+    pass
+
 
 class Flower(Plant):
-    alive = False
+    pass
 
 
 class Fruit(Plant):
     edible = True
-
 
 
 a1 = Predator('Волк с Уолл-Стрит')
